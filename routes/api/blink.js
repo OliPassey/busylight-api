c500231c89670aa9380022a2324d870477901397
Oli Passey <oli@hedgerows.net>
2019-08-06 11:07:02 +0100

    Testing duration of blink

--- routes/api/blink.js
+++ routes/api/blink.js
@@ -3,6 +3,9 @@ const busylight = require('busylight').get();
 module.exports = (req, res) => {
   // Ring and blink
   busylight.ring('Funky').blink(['red', 'yellow', 'blue', 'green'], 150);
-
+  // Turn off ring and blink after 5000ms
+  setTimeout(() => {
+    busylight.ring(false).blink(false);
+}, 5000);
   res.json({ message: 'Busylight should ring and blink' });
 };
